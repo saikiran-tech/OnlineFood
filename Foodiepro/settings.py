bfbe52aab08a213f488f19399d736e5a26161461
saikiran-tech <kiransai00982@gmail.com>
2025-08-07 18:27:23 +0530

    f

--- Foodiepro/settings.py
+++ Foodiepro/settings.py
@@ -147,7 +147,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 # STATIC_URL = '/static/'
-# STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 # STATICFILES_DIRS = [
 #     os.path.join(BASE_DIR,'static')
 
@@ -155,7 +155,7 @@ USE_TZ = True
 
 #media files
 # MEDIA_URL = '/media/'
-# MEDIA_ROOT = os.path.join(BASE_DIR,'media')
+MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
